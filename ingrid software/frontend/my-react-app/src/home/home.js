dd36d2fb40cfb1f2a106c03557b534d8b0c89bdf
SK Sidratul Islam Priyo <lcz982@usask.ca>
2026-03-08 21:31:02 -0600

    debugging some extra lines of code in home.js

--- ingrid software/frontend/my-react-app/src/home/home.js
+++ ingrid software/frontend/my-react-app/src/home/home.js
@@ -1,20 +1,15 @@
-<<<<<<< HEAD
 import Navbar from "./navbar";
 import Main from "./main";
 import SearchBar from "./searchBar";
 import Features from "./features";
 import Footer from "./footer";
 import PropertyList from "./PropertyList";
-=======
 import React, { useState } from "react";
 import Navbar from  "./navbar";
 import Main from "./main";
-import SearchBar from "./searchbar";
 import Features from "./features";
 import Footer from "./footer";
 import "./home.css";
-import PropertyList from "./propertyList";
->>>>>>> origin/nelson-feature
 function Home() {
   const [properties, setProperties] = useState([]);
 
