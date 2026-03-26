6560b6d6b28ab533fe5c68f02e99f4faa559fc4d
basisahmusarrat <fkv482@usask.ca>
2026-03-26 07:29:22 -0600

    added const for checkin, checkout guests and booking message

--- ingrid software/frontend/my-react-app/src/listingpage/propertydetail.js
+++ ingrid software/frontend/my-react-app/src/listingpage/propertydetail.js
@@ -1,10 +1,11 @@
 import React, { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useParams, Navigate } from "react-router-dom";
 import axios from "axios";
 import "./listing.css";
 
 export default function PropertyDetail() {
   const { id } = useParams();
+  const [navigate] = useState(Navigate);
   const [property, setProperty] = useState(null);
   const [furniture, setFurniture] = useState([]);
 
