b2c3c95d2d5c07ad36b7cc8be72c6929e1fcfc71
basisahmusarrat <fkv482@usask.ca>
2026-04-01 22:29:51 -0600

    change the typo for css import

--- ingrid software/frontend/my-react-app/src/pages/payment.js
+++ ingrid software/frontend/my-react-app/src/pages/payment.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 import axios from "axios";
-import "./Payment.css";
+import "./payment.css";
 
 export default function Payment() {
   const location = useLocation();
